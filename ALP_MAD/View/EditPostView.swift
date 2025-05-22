cfafae5d978087821819941294bf98dcd6478333
More <ialessio@student.ciputra.ac.id>
2025-05-22 13:54:09 +0700

    Added both createpost and editpost view. button for lost still needs to be changed to red

---
+++ ALP_MAD/View/EditPostView.swift
@@ -0,0 +1,18 @@
+//
+//  EditPostView.swift
+//  ALP_MAD
+//
+//  Created by student on 22/05/25.
+//
+
+import SwiftUI
+
+struct EditPostView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditPostView()
+}
