f0fbc4f6c2adbcd129688ca5c01ac53f5645820c
Stefanie Agahari <stefanie.agahari@gmail.com>
2025-05-22 09:02:50 +0700

    Initial Commit

---
+++ ALP_MAD/ALP_MADApp.swift
@@ -0,0 +1,17 @@
+//
+//  ALP_MADApp.swift
+//  ALP_MAD
+//
+//  Created by student on 22/05/25.
+//
+
+import SwiftUI
+
+@main
+struct ALP_MADApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ALP_MAD/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ALP_MAD
+//
+//  Created by student on 22/05/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
